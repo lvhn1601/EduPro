cd300aa9900d2a53b47bc5275d90c6635c6ed3f2
lvhn1601 <lvhn1601@gmail.com>
2023-09-26 01:49:04 +0700

    test

--- LearningManagement/src/java/connection/DBContext.java
+++ LearningManagement/src/java/connection/DBContext.java
@@ -12,13 +12,9 @@ public class DBContext {
     public DBContext() {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
-<<<<<<< HEAD
-            String url = "jdbc:mysql://localhost:3306/edupro";
-=======
             String url = "jdbc:mysql://localhost:3306/eduprodb";
->>>>>>> 2028ffe5263447c6db5043b271cdf130efd65724
             String username = "root";
-            String password = "123456";
+            String password = "160103";
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, username, password);
             if (connection != null) {
